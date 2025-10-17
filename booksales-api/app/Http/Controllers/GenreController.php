55ad0b7936d97b65533cb912e3c49dd1aa5e9477
Aryoabdi <aryoabdi39@gmail.com>
2025-10-17 19:32:08 +0700

    first commit

--- booksales-api/app/Http/Controllers/GenreController.php
+++ booksales-api/app/Http/Controllers/GenreController.php
@@ -51,4 +51,75 @@ class GenreController extends Controller
             'data' => $genre
         ], 201);
     }
+
+    public function show(string $id) {
+        $genre = Genre::find($id);
+
+        if (!$genre) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Resource not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Get detail resource',
+            'data' => $genre
+        ], 200);
+    }
+
+    public function update(string $id, Request $request) {
+        $genre = Genre::find($id);
+
+        if (!$genre) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Resource not found'
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'description' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()
+            ], 422);
+        }
+
+        $data = [
+            'name' => $request->name,
+            'description' => $request->description,
+        ];
+
+        $genre->update($data);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Resource updated successfully',
+            'data' => $genre
+        ]);
+    }
+
+    public function destroy(string $id) {
+        $genre = Genre::find($id);
+
+        if (!$genre) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Resource not found'
+            ], 404);
+        }
+
+        $genre->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Delete resource successfully'
+        ], 200);
+    }
 }
--- booksales-api/routes/api.php
+++ booksales-api/routes/api.php
@@ -10,11 +10,28 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::apiResource('authors', AuthorController::class);
+Route::apiResource('books', BookController::class);
+Route::apiResource('genres', GenreController::class);
+
+/*
 Route::get('/authors', [AuthorController::class, 'index']);
 Route::post('/authors', [AuthorController::class, 'store']);
+Route::get('/authors/(id)', [AuthorController::class, 'show']);
+Route::put('/authors/(id)', [AuthorController::class, 'update']);
+Route::delete('/authors/(id)', [AuthorController::class, 'destroy']);
 
 Route::get('/books', [BookController::class, 'index']);
 Route::post('/books', [BookController::class, 'store']);
+Route::get('/books/(id)', [BookController::class, 'show']);
+Route::put('/books/(id)', [BookController::class, 'update']);
+Route::delete('/books/(id)', [BookController::class, 'destroy']);
+
 
 Route::get('/genres', [GenreController::class, 'index']);
 Route::post('/genres', [GenreController::class, 'store']);
+Route::get('/genres/(id)', [GenreController::class, 'show']);
+Route::put('/genres/(id)', [GenreController::class, 'update']);
+Route::delete('/genres/(id)', [GenreController::class, 'destroy']);
+*/
+
